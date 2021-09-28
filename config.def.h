904519577da611e61d85e35dae5fc4812baae337
joeldba <joeldba@github.com>
2021-09-27 23:15:17 -0700

    Fixed fonts

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:pixelsize=13:antialias=true:autohint=true";
+static char *font = "Terminus:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 15;
 
 /*
@@ -136,7 +136,7 @@ static unsigned int defaultrcs = 257;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 4;
 
 /*
  * Default columns and rows numbers
